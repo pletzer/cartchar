02172445163245c0eff096ef8447037d52bef819
Alex Pletzer <alexander@gokliya.net>
2020-11-26 13:58:16 +1300

    now choosing between contours

--- scatter/scatter.py
+++ scatter/scatter.py
@@ -6,45 +6,28 @@ import defopt
 from pathlib import Path
 import pandas
 import os
+import random
 
 
-def run(*, contour_csv_file : Path='', lmbda : float=0.5, nx : int=10, ny : int=10):
+def run(*, contour_csv_dir : Path='', output_csv_file : Path='', nexp : int=100, lmbda : float=0.5, nx : int=1, ny : int=2):
     """Run simulation
 
-    :param contour_csv_file: CSV file holding obstacle contour data
+    :param contour_csv_dir: directory holding the countour CSV files
+    :param output_csv_file: output CSV file
+    :param nexp: number of experiments
     :param lmbda: wave length
     :param nx: number of x grid cells
     :param ny: number of y grid cells
     """
 
-    twoPi = 2. * numpy.pi
+    if not str(contour_csv_dir):
+        raise RuntimeError('ERROR must provide directory containing contour CSV files')
+    else:
+        if not contour_csv_dir.exists():
+            raise RuntimeError('ERROR must input directory containing CSV files does not exist!')
 
-    # incident wavenumber
-    knum = twoPi / lmbda
-    kvec = numpy.array([knum, 0.,], numpy.float64)
-    # get the pointers from the numpy arrays
-    kvecPtr = kvec.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-
-    # contour points of the obstacle
-    df = pandas.read_csv(contour_csv_file)
-    xc = numpy.array(df['x'].values, numpy.float64)
-    yc = numpy.array(df['y'].values, numpy.float64)
-    # rescale to wave length
-    xscale = xc.max() - xc.min()
-    yscale = yc.max() - yc.min()
-    # want each obstacle to be about 5 wavelengths
-    factor = (5*lmbda) / xscale
-    xc *= factor
-    yc *= factor
-    nc1 = len(xc)
-    xcPtr = xc.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-    ycPtr = yc.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
-
-    # create grid 
-    xmin, xmax = xc.min() - 5*lmbda, xc.max() + 3*lmbda
-    ymin, ymax = yc.min() - 3*lmbda, yc.max() + 4*lmbda
-    xg = numpy.linspace(xmin, xmax, nx + 1)
-    yg = numpy.linspace(ymin, ymax, ny + 1)
+    if str(output_csv_file) == '.':
+        raise RuntimeError('ERROR must provide output CSV file name')
 
     # find the library under the build directory
     waveLibFile = ''
@@ -79,29 +62,89 @@ def run(*, contour_csv_file : Path='', lmbda : float=0.5, nx : int=10, ny : int=
                                              doubleStarType,
                                              doubleStarType]
 
-    # compute the field
-    scat = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
-    inci = numpy.zeros((ny + 1, nx + 1), numpy.complex64)
+    # constants
+    twoPi = 2. * numpy.pi
+
+    # incident wavenumber
+    knum = twoPi / lmbda
+    kvec = numpy.array([knum, 0.,], numpy.float64)
+    # get the pointers from the numpy arrays
+    kvecPtr = kvec.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+
+    # create grid 
+    xmin, xmax = -10*lmbda, 0.0
+    ymin, ymax = -3.5*lmbda, 4.2*lmbda
+    xg = numpy.linspace(xmin, xmax, nx + 1)
+    yg = numpy.linspace(ymin, ymax, ny + 1)
+
+    # contour points of the obstacle
+    csvFiles = list(contour_csv_dir.glob('*.csv'))
+
+    outputDf = {'iexp': [], 'character': []}
+    count = 0
     for j in range(ny + 1):
-        y = yg[j]
         for i in range(nx + 1):
-            x = xg[i]
+            outputDf[f'scattered_re_{count}'] = []
+            outputDf[f'scattered_im_{count}'] = []
+            count += 1
 
-            # need to check that x,y are outside contour
-            # otherwise continue
-            pPtr = (ctypes.c_double * 2)(x, y)
+    for iexp in range(nexp):
 
-            # skip if point is inside closed contour
-            if wavelib.isInsideContour(pPtr, nc1, xcPtr, ycPtr) == 1:
-                continue
+        csvFile = random.choice(csvFiles)
+        outputDf['character'].append(csvFile.name.split('.')[0])
 
-            wavelib.cincident(kvecPtr, pPtr, ctypes.byref(realVal), ctypes.byref(imagVal))
-            inci[j, i] = realVal.value + 1j*imagVal.value
+        df = pandas.read_csv(csvFile)
+        xc = numpy.array(df['x'].values, numpy.float64)
+        yc = numpy.array(df['y'].values, numpy.float64)
+        # rescale to wave length
+        xscale = xc.max() - xc.min()
+        yscale = yc.max() - yc.min()
+        # want each obstacle to be about 5 wavelengths
+        factor = (10*lmbda) / yscale
+        xc *= factor
+        yc *= factor
 
-            wavelib.computeScatteredWave(kvecPtr, nc1, xcPtr, ycPtr, pPtr, 
-                                         ctypes.byref(realVal), ctypes.byref(imagVal))
-            scat[j, i] = realVal.value + 1j*imagVal.value
+        # random rotation
+        theta = -numpy.pi/2. + numpy.pi*random.random()
 
+        cosTheta = numpy.cos(theta)
+        sinTheta = numpy.sin(theta)
+        xc2 =  cosTheta*xc + sinTheta*yc
+        yc2 = -sinTheta*xc + cosTheta*yc
+        xc[:] = xc2
+        yc[:] = yc2
+
+        # shift to the right location
+        xc -= xc.min()
+        xc += 3.5*lmbda
+        yc -= yc.min()
+        yc -= 2.1*lmbda
+        # number of contour points
+        nc1 = len(xc)
+
+        xcPtr = xc.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+        ycPtr = yc.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+
+        # compute the field
+        count = 0
+        for j in range(ny + 1):
+            y = yg[j]
+            for i in range(nx + 1):
+                x = xg[i]
+                pPtr = (ctypes.c_double * 2)(x, y)
+
+                wavelib.computeScatteredWave(kvecPtr, nc1, xcPtr, ycPtr, pPtr, 
+                                             ctypes.byref(realVal), ctypes.byref(imagVal))
+
+                outputDf[f'scattered_re_{count}'].append(realVal.value)
+                outputDf[f'scattered_im_{count}'].append(imagVal.value)
+                count += 1
+
+        outputDf['iexp'].append(iexp)
+
+    # write the results
+    print(f'writing the results to {output_csv_file}')
+    pandas.DataFrame(outputDf).to_csv(output_csv_file)
 
 if __name__ == '__main__':
     defopt.run(run)
